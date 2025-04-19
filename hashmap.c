9a707b311b02c3fabb77bac0957139e72dbe43cc
Your Name <you@example.com>
2025-04-18 21:52:16 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -73,9 +73,9 @@ void insertMap(HashMap * map, char * key, void * value) {
 Pair * searchMap(HashMap * map,  char * key) {   
     long posi = hash(key,map->capacity);
     //long comprobar = posi;
-    if(map->buckets[posi]->key != NULL || is_equal(map->buckets[posi]->key,key)){
+    if(map->buckets[posi]->key != NULL && is_equal(map->buckets[posi]->key,key)){
         map->current = posi;
-        map->size++;
+        return map->current[posi];
         posi = (posi + 1) % map->capacity;
         
     }
